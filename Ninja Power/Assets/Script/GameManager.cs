791d2ead034abd6dc1a6191ea10b24eed7cb855c
Gabriel Marcoccia <42481520+Gabrielmarcoccia36@users.noreply.github.com>
2020-08-04 01:52:27 -0400

    ASD :v

--- Ninja Power/Assets/Script/GameManager.cs
+++ Ninja Power/Assets/Script/GameManager.cs
@@ -4,15 +4,8 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private float level = 1;
+    private float experience = 0;
+    private float maxExp = 10;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
