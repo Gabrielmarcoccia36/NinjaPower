7a863706456af607c6ea486f818d7722adc2f103
Gabriel Marcoccia <42481520+Gabrielmarcoccia36@users.noreply.github.com>
2020-08-05 00:14:34 -0400

    Added a bunch of UI stuff

--- Ninja Power/Assets/Script/MainCanvas.cs
+++ Ninja Power/Assets/Script/MainCanvas.cs
@@ -7,13 +7,48 @@ public class MainCanvas : MonoBehaviour
 {
     private GameManager gameManager;
 
+    // Data
+    [Header("Data")]
     [SerializeField]
-    private TextMeshProUGUI levelUI, experienceUI, vipUI, powerUI, goldUI, diamondsUI;
+    private TextMeshProUGUI levelUI;
+    [SerializeField]
+    private TextMeshProUGUI experienceUI;
+    [SerializeField]
+    private TextMeshProUGUI vipUI;
+    [SerializeField]
+    private TextMeshProUGUI powerUI;
+    [SerializeField]
+    private TextMeshProUGUI goldUI;
+    [SerializeField]
+    private TextMeshProUGUI diamondsUI;
+
     private int[] data;
+
+    // Panels
+    [Header("Panels")]
+    [SerializeField]
+    private GameObject MainPanel;
+    [SerializeField]
+    private GameObject BagPanel;
+    [SerializeField]
+    private GameObject VillagePanel;
+    [SerializeField]
+    private GameObject NinjaPanel;
+    [SerializeField]
+    private GameObject ClonePanel;
+
+    private GameObject curPanel;
+
+    [Header("Buttons")]
+    [SerializeField]
+    private GameObject fightButton;
 
     private void Awake()
     {
         gameManager = FindObjectOfType<GameManager>();
+        curPanel = MainPanel;
+        fightButton.SetActive(false);
+
     }
 
     private void Update()
@@ -31,4 +66,58 @@ public class MainCanvas : MonoBehaviour
         goldUI.text = "Gold " + data[4];
         diamondsUI.text = "Diamonds " + data[5];
     }
+
+    public void SetFightButton(bool ready)
+    {
+        if (ready)
+        {
+            fightButton.SetActive(true);
+        }
+        else
+        {
+            fightButton.SetActive(false);
+        }
+    }
+
+    #region ButtonFunctions
+    public void OnBagClicked()
+    {
+        curPanel.SetActive(false);
+        BagPanel.SetActive(true);
+        curPanel = BagPanel;
+    }
+
+    public void OnVillageClicked()
+    {
+        curPanel.SetActive(false);
+        VillagePanel.SetActive(true);
+        curPanel = VillagePanel;
+    }
+
+    public void OnNinjaClicked()
+    {
+        curPanel.SetActive(false);
+        NinjaPanel.SetActive(true);
+        curPanel = NinjaPanel;
+    }
+
+    public void OnCloneClicked()
+    {
+        MainPanel.SetActive(false);
+        ClonePanel.SetActive(true);
+        curPanel = ClonePanel;
+    }
+
+    public void OnBackClicked()
+    {
+        curPanel.SetActive(false);
+        MainPanel.SetActive(true);
+        curPanel = MainPanel;
+    }
+
+    public void OnFightClicked()
+    {
+
+    }
+    #endregion
 }
