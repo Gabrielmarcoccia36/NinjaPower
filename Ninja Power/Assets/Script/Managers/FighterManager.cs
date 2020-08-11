20cd1595583074048de65ce154d7cdde8499d6b6
Gabriel Marcoccia <42481520+Gabrielmarcoccia36@users.noreply.github.com>
2020-08-11 01:04:07 -0400

    Submitting progress of fighters, lineup, and more shit I don't remember

---
+++ Ninja Power/Assets/Script/Managers/FighterManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FighterManager : MonoBehaviour
+{
+    [Header("Lineup")]
+    [SerializeField]
+    private GameObject[] buttons;
+    public CharacterStats[] lineupCharacter;
+
+    [Header("Fighter Options")]
+    [SerializeField]
+    private GameObject[] options;
+    [SerializeField]
+    private CharacterStats[] fighterOptions;
+
+}
